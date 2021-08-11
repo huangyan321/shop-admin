58c34e4a897d28bf19286cf7d1beeb3d2d7b244b
huangyan321 <1604549268@qq.com>
2021-08-12 01:10:35 +0800

    fix

--- src/utils/usual.js
+++ src/utils/usual.js
@@ -9,6 +9,21 @@ export function getDomain() {
   const url = window.location.href;
   const i = find(url, ':', 1);
   let domain = url.substr(0, i);
-  domain = process.env.NODE_ENV === 'development' ? "http://121.196.101.73:8888" : "http://127.0.0.1:8888";
+  // domain = process.env.NODE_ENV === 'development' ? "http://121.196.101.73:8888" : "http://127.0.0.1:8888";
+  domain = "http://121.196.101.73:8888"
   return domain + "/api/private/v1/"
+}
+export function fetchInit() {
+  const find = function (str, symbol, num) {
+    var x = str.indexOf(symbol);
+    for (var i = 0; i < num; ++i) {
+      x = str.indexOf(symbol, ++x)
+    }
+    return x
+  }
+  const url = window.location.href;
+  const i = find(url, ':', 1);
+  let domain = url.substr(0, i);
+  domain =  process.env.NODE_ENV === 'development' ? "http://121.196.101.73:8888" : "http://127.0.0.1:8888" + domain;
+  return domain
 }
