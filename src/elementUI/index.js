0093e9406a81a958df12ad257d0bb0a87bd4dd89
huangyan321 <1604549268@qq.com>
2021-08-12 12:04:47 +0800

    按需引入element

---
+++ src/elementUI/index.js
@@ -0,0 +1,70 @@
+import Vue from 'vue';
+import { Breadcrumb,
+  BreadcrumbItem,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  Icon,
+  MenuItem,
+  Scrollbar,
+  Menu,
+  Submenu,
+  Upload,
+  Form,
+  Input,
+  Select,
+  Option,
+  Col,
+  TimePicker,
+  FormItem,
+  Switch,
+  Checkbox,
+  RadioGroup,
+  Radio,
+  Button,
+  Row,
+  Alert,
+  TableColumn,
+  Tag,
+  Tree,
+  Loading,
+  MessageBox,
+  Message,
+  Notification
+} from "element-ui"
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Dropdown)
+Vue.use(DropdownMenu)
+Vue.use(DropdownItem)
+Vue.use(Icon)
+Vue.use(MenuItem)
+Vue.use(Scrollbar)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(Upload)
+Vue.use(Form)
+Vue.use(Input)
+Vue.use(Select)
+Vue.use(Option)
+Vue.use(Col)
+Vue.use(TimePicker)
+Vue.use(FormItem)
+Vue.use(Switch)
+Vue.use(Checkbox)
+Vue.use(RadioGroup)
+Vue.use(Radio)
+Vue.use(Button)
+Vue.use(Row)
+Vue.use(Alert)
+Vue.use(Row)
+Vue.use(TableColumn)
+Vue.use(Tag)
+Vue.use(Tree)
+Vue.prototype.$loading = Loading.service;
+Vue.prototype.$msgbox = MessageBox;
+Vue.prototype.$alert = MessageBox.alert;
+Vue.prototype.$confirm = MessageBox.confirm;
+Vue.prototype.$prompt = MessageBox.prompt;
+Vue.prototype.$notify = Notification;
+Vue.prototype.$message = Message;
--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 import Vue from 'vue'
 import 'normalize.css/normalize.css' // A modern alternative to CSS resets
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+// import locale from 'element-ui/lib/locale/lang/en' // lang i18n
 import '@/styles/index.scss' // global css
+import './elementUI'
 import App from './App'
 import store from './store'
 import router from './router'
@@ -25,9 +24,9 @@ import '@/permission' // permission control
 // }
 
 // set ElementUI lang to EN
-Vue.use(ElementUI, { locale })
-// 如果想要中文版 element-ui，按如下方式声明
-// Vue.use(ElementUI)
+// Vue.use(ElementUI, { locale })
+// // 如果想要中文版 element-ui，按如下方式声明
+// // Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
